0ca5fc3d33a8758385bc995355811aca4bf2b487
didiermauricio~ <didiermauricio@gmail.com~>
2025-09-02 23:35:24 +0200

    2nd test

--- cypress/e2e/Features/moodysLogin.feature
+++ cypress/e2e/Features/moodysLogin.feature
@@ -1,9 +1,18 @@
 Feature: Moodys Login
 
-Scenario: Login Page with empty credentials
+Scenario: Login Page with empty email field
     Given I access to login page
     And I accept the cookies
     When I click on sign in button
     And I click on next button
+    And I leave the username field empty
+    Then I should see the error message "Required"
+
+Scenario: Login Page with wrong email field
+    Given I access to login page
+    And I accept the cookies
+    When I click on sign in button
+    And I click on next button
+    And I fill the username field with a wrong value
     Then I should see the error message "Required"
     
--- cypress/e2e/Steps/loginSteps.js
+++ cypress/e2e/Steps/loginSteps.js
@@ -1,6 +1,16 @@
 
 import { Given, And, When, Then } from "cypress-cucumber-preprocessor/steps";
 
+const loginBaseUrl = "https://login.moodys.com/";
+const locators = {
+  acceptCookiesButton: '#onetrust-accept-btn-handler',
+  signInButton: '[data-testid="sign-in"]',
+  nextButton: '[type="submit"]',
+  emailField: '[inputmode="email"]',
+  errorMessage: '[data-slot="error"]',
+  acceptCookiesButton: '#onetrust-accept-btn-handler'
+};
+
 Given('I access to login page', () => {
   cy.visit("/")
 
@@ -8,28 +18,34 @@ Given('I access to login page', () => {
 
 And('I accept the cookies', () => {
   cy.get("body").then(($body) => {
-     cy.get("#onetrust-accept-btn-handler",{timeout:10000}).click();    
+     cy.get(locators.acceptCookiesButton,{timeout:10000}).click();    
   });
 });
 
 When('I click on sign in button', () => {
-  cy.get('[data-testid="sign-in"]').should("be.visible").click();
+  cy.get(locators.signInButton).should("be.visible").click();
 })
 
 And('I click on next button', () => {
-  cy.origin('https://login.moodys.com/', () => {
-    cy.get('[type="submit"]',{timeout:20000}).click();
+  cy.origin(loginBaseUrl,{args:{locators}}, ({locators}) => {
+    cy.get(locators.nextButton,{timeout:20000}).click();
   });
 })
 
-And('I leave the username and password fields empty', () => {
-  cy.origin('https://login.moodys.com/', () => {
-    cy.get('[inputmode="email"]').should("be.visible");
+And('I leave the username field empty', () => {
+  cy.origin(loginBaseUrl,{args:{locators}}, ({locators}) => {
+    cy.get(locators.emailField).should("be.visible");
+  } );
+ })
+
+ And('I fill the username field with a wrong value', () => {
+  cy.origin(loginBaseUrl,{args:{locators}}, ({locators}) => {
+    cy.get(locators.emailField).should("be.visible").type("12345");
   } );
  })
 
 Then('I should see the error message {string}', (errorMessage) => {
-  cy.origin('https://login.moodys.com/',{args:{error:errorMessage}}, ({error}) => {
-    cy.get('[data-slot="error"]').should("be.visible").and('have.text', error);
+  cy.origin(loginBaseUrl,{args:{errorMessage, locators}}, ({errorMessage,locators}) => {
+    cy.get(locators.errorMessage).should("be.visible").and('have.text', errorMessage);
   } );
 })
